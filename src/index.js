7ab977ae8cec3a80c3cbf3b649493f57df7162fd
DryByte <93609026+DryByte@users.noreply.github.com>
2022-07-02 16:35:02 -0300

    Use full path to import things in the right way

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-exports.Client = require("./client/Client.js");
-exports.Utils = require("./utils.js");
-exports.VXL = require("./vxl/VXL.js");
-exports.Types = require("./types");
+exports.Client = require(__dirname+"/client/Client.js");
+exports.Utils = require(__dirname+"/utils.js");
+exports.VXL = require(__dirname+"/vxl/VXL.js");
+exports.Types = require(__dirname+"/types");
 
 // POG part, need to change later...
 const fs = require("fs");
-let packetsNames = fs.readdirSync("./src/packets");
+let packetsNames = fs.readdirSync(__dirname+"/packets");
 let packetsObj = {};
 
 for (let name of packetsNames) {
-	packetsObj[name.slice(0, -3)] = require(`./packets/${name}`);
+	packetsObj[name.slice(0, -3)] = require(__dirname+`/packets/${name}`);
 }
 
 exports.Packets = packetsObj;
